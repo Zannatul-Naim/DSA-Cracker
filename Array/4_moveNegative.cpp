eeaaa9ddcc25d635788a9972c2dd7358ff2e6444
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-03-06 23:43:54 +0600

    Move all negative elements to end
    Not exact answer but correct

---
+++ Array/4_moveNegative.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        
+        //sort(a, a+n);
+        
+        int b[n];
+        int j = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(a[i] >= 0) {
+                b[j] = a[i];
+                j++;
+            }
+        }
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(a[i] < 0) {
+                b[j] = a[i];
+                j++;
+            }
+        }
+        
+        for(int i = 0; i < n; i++)
+        {
+            cout << b[i] << " ";
+        }
+        
+        cout << "\n";
+    }
+    return 0;
+}
